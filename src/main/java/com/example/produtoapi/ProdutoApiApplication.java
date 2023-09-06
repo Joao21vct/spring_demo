7b725eabd9331d81db9441f6b250f79cff388fd2
unknown <joao.silva@germinare.org.br>
2023-09-06 18:50:33 -0300

    alteração na main

--- src/main/java/com/example/produtoapi/ProdutoApiApplication.java
+++ src/main/java/com/example/produtoapi/ProdutoApiApplication.java
@@ -2,6 +2,7 @@ package com.example.produtoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class ProdutoApiApplication {
@@ -9,5 +10,8 @@ public class ProdutoApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProdutoApiApplication.class, args);
     }
-
+    @GetMapping("/")
+    public String funciona(){
+        return "EU SOU MT BOM";
+    }
 }
